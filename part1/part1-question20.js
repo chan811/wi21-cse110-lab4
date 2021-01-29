5db54fdbd5b6c894dbbb5226f6ce9c0739d22305
chan811 <56621488+chan811@users.noreply.github.com>
2021-01-28 23:56:57 -0800

    Update part1-question20.js

--- part1/part1-question20.js
+++ part1/part1-question20.js
@@ -1,5 +1,6 @@
-setInterval(printTime(){
+setInterval(printTime, 1000); //one second is 1000ms
+function printTime(){
   let d = new Date();
   let time = d.toLocaleTimeString();
   console.log(time);
-}, 1000); //one second is 1000ms
+}
